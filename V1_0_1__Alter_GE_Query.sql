12262a9c12d3216a87638b2ab6bcbea4bf5db0a3
eitjagadeshan <jagadeshah@eitworks.com>
2020-03-12 18:26:37 +0530

    alter 1.0.1

--- V1_0_1__Alter_GE_Query.sql
+++ V1_0_1__Alter_GE_Query.sql
@@ -1,4 +1,4 @@
-NSERT INTO `ltmscompanyadmindb`.`applicationsettings` (`key`, `values`) VALUES ('TrackNowApi', 'https://tracknow.apmkingstrack.com/tracknow/');
+INSERT INTO `ltmscompanyadmindb`.`applicationsettings` (`key`, `values`) VALUES ('TrackNowApi', 'https://tracknow.apmkingstrack.com/tracknow/');
 
 CREATE TABLE `fleettrackingdb`.`tracknow` ( `id` VARCHAR(50) NOT NULL, `vin` VARCHAR(30) NOT NULL, `companyId` VARCHAR(30) NOT NULL, `userId` VARCHAR(30) NOT NULL, `linkCreated` DATETIME NOT NULL, `linkExpiry` DATETIME NOT NULL, `imeiNo` VARCHAR(30) NOT NULL, `manufacture` VARCHAR(30) NOT NULL, `model` VARCHAR(20) NOT NULL, `createdBy` VARCHAR(30) NOT NULL, PRIMARY KEY (`id`) );
 
